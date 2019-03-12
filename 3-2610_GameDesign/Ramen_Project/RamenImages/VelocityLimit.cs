43f40c9f32b70bb692d653766fb082db4b1d1d0a
Unknown <ryleyburnett@live.com>
2019-03-12 15:40:01 -0600

    Velocity Limit Script

---
+++ 3-2610_GameDesign/Ramen_Project/RamenImages/VelocityLimit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class VelocityLimit : MonoBehaviour
+{
+	public Rigidbody ropeRigBod;
+
+	public int velLimit;
+
+	
+
+		private void Start()
+		{
+			ropeRigBod = gameObject.GetComponent<Rigidbody>();
+		}
+
+		void Update()
+		{
+			// Trying to Limit Speed
+			if(ropeRigBod.velocity.magnitude > velLimit){
+				ropeRigBod.velocity = Vector3.ClampMagnitude(ropeRigBod.velocity, velLimit);
+			}
+		}
+ 
+		void OnGUI()
+		{
+			GUI.Label(new Rect(20, 20, 200, 200), "rigidbody velocity: " + ropeRigBod.velocity);
+		}
+}
+
